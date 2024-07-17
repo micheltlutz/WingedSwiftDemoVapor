c890e9952ab74d44e664ac53d34f83ab594deca4
Michel Anderson Lutz Teixeira <michel_lutz@icloud.com>
2024-07-17 09:26:36 -0300

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,19 @@
 import Vapor
+import WingedSwift
 
 func routes(_ app: Application) throws {
     app.get { req async in
         "It works!"
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.get("hello") { req in
+        let helloDiv = Div(content: "Hello World")
+        
+        let response = Response()
+        response.body = .init(string: helloDiv.render()) // Init Body
+        return response.encodeResponse(status: .ok,
+                                       headers: HTTPHeaders.init([("Content-Type", 
+                                                                   "text/html; charset=UTF-8")]),
+                                       for: req)
     }
 }
