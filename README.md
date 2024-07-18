2cba69ac58c0d52af5338f32cacf51bffcd39e51
Michel Anderson Lutz Teixeira <michel_lutz@icloud.com>
2024-07-17 21:14:21 -0300

    deploy to fly.io and doc

--- README.md
+++ README.md
@@ -1,23 +1,32 @@
 # WingedSwift Demo Vapor
 
-## This Demo using
+## This Demo Uses
 
 - [WingedSwift](https://github.com/micheltlutz/Winged-Swift)
 - [Vapor](https://vapor.codes)
 - [Milligram.io](https://milligram.io)
-- [Deploy in fly.io](https://docs.vapor.codes/deploy/fly/)
+- [Deploy on fly.io](https://docs.vapor.codes/deploy/fly/)
 
-To view routes and WingedSwift working open `Sources > App > routes.swift`.
+## See Online Deployment
 
-To rum project in terminal run
+- [https://wingedswiftdemovapor.fly.dev](https://wingedswiftdemovapor.fly.dev)
+- [https://wingedswiftdemovapor.fly.dev/hello](https://wingedswiftdemovapor.fly.dev/hello)
+
+To view the routes and see WingedSwift in action, open `Sources > App > routes.swift`.
+
+To run the project in the terminal, use the following command:
 
 ```bash
 swift run
 ```
 
+After running the command, you should see the following notice:
+
 ```bash
 [ NOTICE ] Server started on http://127.0.0.1:8080
 ```
 
-- [open localhost](http://127.0.0.1:8080)
+Now you can open the following URLs in your browser:
 
+- [Open localhost](http://127.0.0.1:8080)
+- [Open localhost/hello](http://127.0.0.1:8080/hello)
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,17 +2,44 @@ import Vapor
 import WingedSwift
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
-
-    app.get("hello") { req in
+    app.get { req in
         let helloDiv = Div(content: "Hello World")
         
         let response = Response()
         response.body = .init(string: helloDiv.render()) // Init Body
         return response.encodeResponse(status: .ok,
-                                       headers: HTTPHeaders.init([("Content-Type", 
+                                       headers: HTTPHeaders.init([("Content-Type",
+                                                                   "text/html; charset=UTF-8")]),
+                                       for: req)
+    }
+
+    app.get("hello") { req in
+        let document = html {
+            Head(children: [
+                Meta(name: "description", content: "A description of the page"),
+                Link(href: "https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css", rel: "stylesheet")
+            ])
+            Body(children: [
+                Header(children: [
+                    Nav(children: [
+                        A(href: "#home", content: "Home"),
+                        A(href: "#about", content: "About"),
+                        A(href: "#contact", content: "Contact")
+                    ])
+                ]),
+                MainTag(children: [
+                    P(content: "Welcome to our website!")
+                ]),
+                Footer(children: [
+                    P(content: "© 2024 Company, Inc.")
+                ])
+            ])
+        }
+
+        let response = Response()
+        response.body = .init(string: document.render()) // Init Body
+        return response.encodeResponse(status: .ok,
+                                       headers: HTTPHeaders.init([("Content-Type",
                                                                    "text/html; charset=UTF-8")]),
                                        for: req)
     }
